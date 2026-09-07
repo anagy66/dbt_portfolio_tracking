1639058a52e042e4cda97ceb7ebebb4dbc889add
anagy66 <a.nagy66@yahoo.ca>
2026-09-07 15:59:56 -0600

    DEV dbt: Completed the staging model

--- models/staging/stg_portfolio.sql
+++ models/staging/stg_portfolio.sql
@@ -6,24 +6,36 @@ source as (
 
 ),
 
-final as (
+renamed as (
 
     select
-        accountID::varchar as account_id,
-        symbol::varchar as symbol,
-        description::varchar as description,
-        exchange::varchar as exchange,
+        accountID::varchar as account_code,
+        symbol::varchar as security_code,
+        description::varchar as security_name,
+        exchange::varchar as exchange_code,
         report_date::date as report_date,
         quantity::integer as quantity,
         cost_base::numeric(12, 2) as cost_base,
         position_value::numeric(12, 2) as position_value,
-        currency::varchar as currency,
-        
-        (position_value - cost_base)::numeric(12, 2) as unrealized_profit,
-        (unrealized_profit / nullif(cost_base, 0) * 100)::numeric(12, 2) as unrealized_profit_pct
+        currency::varchar as currency_code,
+
+        'raw.dbt_portfolio_tracking'::varchar as record_source,
 
     from source
 
+),
+
+hashed as (
+
+    select
+        concat_ws('|', account_code, security_code) as position_hkey,
+        concat_ws('|', account_code, security_code, security_name, exchange_code, 
+            report_date, quantity, cost_base, position_value, currency_code) as position_hdiff,
+        *,
+        '{{ run_started_at }}'::timestamp as load_ts_utc
+
+    from renamed
+    
 )
 
-select * from final
+select * from hashed
